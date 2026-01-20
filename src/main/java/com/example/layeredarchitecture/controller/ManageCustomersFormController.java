66e138783e5ef3ed1e5492813f9584bd2b40d6af
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-01-20 14:39:22 +0530

    initial commit

--- src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
@@ -181,10 +181,12 @@ public class ManageCustomersFormController {
 
 
     boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
-        pstm.setString(1, id);
-        return pstm.executeQuery().next();
+
+        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+        customerDAO.existCustomer(id);
+
+        return false;
+
     }
 
 
--- src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -40,5 +40,10 @@ public class CustomerDAOImpl {
         pstm.executeUpdate();
     }
 
-    public void exitCustomer(String id) throws SQLException, ClassNotFoundException {}
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
+        pstm.setString(1, id);
+        return pstm.executeQuery().next();
+    }
 }
